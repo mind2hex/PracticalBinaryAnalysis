8fed3837758624152d228796f7b1b465449c3ff5
mind2hex <neodeus8@gmail.com>
2021-03-06 14:13:35 -0500

    function DOS_HEADER_emagic finished, working with DOS_HEADER_lfanew

--- PE_format/anpe.c
+++ PE_format/anpe.c
@@ -2,8 +2,10 @@
 #include<stdint.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
 
 /*
+  author [mind2hex - Johan Alexis]
   anpe v1.0
   Program to analyze PE files ( Portable Executable ) 
  
@@ -14,68 +16,71 @@
 
 */
 
+int check_file_existence(int8_t *filename);
+
+// DOS HEADER
+int DOS_HEADER_emagic(FILE *fptr); // e_magic  MZ 0x4d 0x5a
+int DOS_HEADER_lfanew(FILE *fptr); // l_fanew  read pointer from position 0x3c
+
+
 int exception(int8_t *, int8_t);
-int check_PE(int8_t *);
 
 int main(){
   int8_t program_status = 0;
+  int8_t *filename = "EasyPass.exe";
+  FILE *fptr = fopen(filename, "rb");
 
-  // Checking if the file is an PE
-  program_status = check_PE("EasyPass.exe");
-  if ( program_status != 0)
-    return program_status
   
+  program_status=check_file_existence(filename);
+  if (program_status != 0)
+    return program_status;
 
-  FILE *PE_file;
-  PE_file = fopen("EasyPass.exe", "rb");
   
-  // Checking file existence
-  if ( PE_file == NULL ){
-    program_status = exception(" File doesn't exist ", 1);
-    if ( program_status != 0 )
-      return program_status;
-  }
+  puts("-----INSPECTING HEADER");
+  program_status=DOS_HEADER_emagic(fptr);
+  if (program_status != 0)
+    return program_status;
 
-  // Checking MZ DOS HEADER
-  unsigned char *buffer;
-  buffer = malloc(2);
-  fread(buffer, sizeof *buffer, 2, PE_file);
-  fseek(PE_file, 0, SEEK_SET);
-  // sizeof *buffer == 8 i don't know why.
+  program_status=DOS_HEADER_lfanew(fptr);
+  if (program_status != 0)
+    return program_status;
+
+  return 0;
+}
+
+int check_file_existence(int8_t *filename){
+  if ( access(filename, F_OK) != 0 )
+    // returning exit code 1 - File doesn't exist
+    return exception("File doesn't exist", 1);
   
-  if ( strcmp(buffer, "MZ") != 0){
-    program_status = exception(" Not a PE file ", 2);
-    if ( program_status != 0 )
-      return program_status;
+  return 0;
+}
+
+int DOS_HEADER_emagic(FILE *fptr){
+  uint8_t *buffer = malloc(sizeof *buffer * 2);
+  fread(buffer, 1, 2, fptr);
+  if (strcmp("MZ", buffer) != 0){
+    // returning exit code 2 - File isn't a PE
+    return exception("File isn't a PE32", 2);
   }else{
-    printf("[1] HEADER: %s ( Mark Zbikowsky )\n", buffer);
+    printf("[1] e_magic: ASCII(%s) HEX(4d 5a) (Mark Zbikowsky)\n", buffer);
+    free(buffer);
+    return 0;
   }
-  
-
-  fclose(PE_file);  
-  return 0;
-
 }
 
-int check_PE(int8_t * filename){
-  unsigned char *buffer;
-  buffer = malloc(2);
-  FILE *fptr;
-  fptr = fopen(filename, "rb");
-  fread(buffer, sizeof *buffer, 2, fptr);
-  fclose(fptr);
+int DOS_HEADER_lfanew(FILE *fptr){
+  uint8_t *buffer = malloc(sizeof *buffer * 2);
+  fseek(fptr, 0x3c, 0);
+  printf("Position of pointer: %x\n",ftell(fptr));
+  fread(buffer, 1, 2, fptr);
+  printf("Position of pointer: %x\n",ftell(fptr));
 
-  if ( strcmp(buffer, "MZ") != 0)
-    return exception(" Not a PE file ", 2);
-
+  printf(" %x \n", buffer);
   return 0;
 }
 
-int exception(int8_t * reason, int8_t return_code){
-  puts("[X] ERROR...");
-  printf("[!] Reason: %s\n", reason);  
-  printf("[!] Return code %d\n", return_code);
-  return return_code;
+int exception(int8_t *reason, int8_t status){
+  printf("[X] ERROR: %s\n",reason);
+  return status;
 }
-
-    
